1251fc599edf5e2e5198b2b93a8642b4c69fd1e7
henrike78 <henrike78@gmail.com>
2026-01-27 10:32:18 +0100

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -1,48 +1,34 @@
 import Link from 'next/link';
-import Image from 'next/image';
 
 export default function Header() {
   return (
-    <header>
-      <nav>
-        <Link href="/">
-          <a className="logo" aria-label="H&C Clean Pro - Inicio">
-            <Image
-              src="/logo.png"
-              alt="H&C Clean Pro - Servicios de Limpieza"
-              width={300}
-              height={300}
-            />
-          </a>
-        </Link>
-        <ul>
-          <li>
-            <Link href="/">
-              <a>Inicio</a>
-            </Link>
-          </li>
-          <li>
-            <Link href="/services">
-              <a>Servicios</a>
-            </Link>
-          </li>
-          <li>
-            <Link href="/trabajos">
-              <a>Trabajos</a>
-            </Link>
-          </li>
-          <li>
-            <Link href="/about">
-              <a>Sobre Nosotros</a>
-            </Link>
-          </li>
-          <li>
-            <Link href="/contact">
-              <a>Contacto</a>
-            </Link>
-          </li>
-        </ul>
-      </nav>
+    <header className="site-header">
+      <div className="header-inner">
+        {/* Logo */}
+        <a href="/" className="logo">
+          <img
+            src="/logo-hc-clean-pro.png" // pon aquí el nombre real de tu archivo de logo en /public
+            alt="H&C Clean Pro - Servicios de Limpieza"
+            className="logo-image"
+          />
+        </a>
+
+        {/* Navegación */}
+        <nav className="main-nav">
+          <Link href="/">
+            <a>Inicio</a>
+          </Link>
+          <Link href="/services">
+            <a>Servicios</a>
+          </Link>
+          <Link href="/about">
+            <a>Sobre Nosotros</a>
+          </Link>
+          <Link href="/contact">
+            <a>Contacto</a>
+          </Link>
+        </nav>
+      </div>
     </header>
   );
 }
